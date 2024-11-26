73d6b743e40fbcfee255eaeec7fd0b8267305168
Jorge AVM <javillamar@eafit.edu.co>
2024-11-26 10:53:23 -0500

    add kssdtree script

---
+++ clustering/networkx_clustering/make_tree.py
@@ -0,0 +1,26 @@
+import kssdtree
+import sys
+
+genomic_dir = sys.argv[1]
+genomic_dir=genomic_dir.rstrip("/")
+
+threshold = "99"
+
+
+shuf_file = "L3K10"
+sketch_dir = "GENOMIC_s" + threshold
+phylip_file = "t" + threshold + ".phylip"
+newick_file = "tree_" + threshold + ".newick"
+
+conf = {
+    "dnj_flag": 1,
+    "dnj_method": "dnj",
+}
+
+
+kssdtree.shuffle(k=10, l=3, s=6, o=shuf_file)
+#kssdtree.sketch(shuf_file=shuf_file, genome_files=genomic_dir, output=sketch_dir)
+#kssdtree.dist(genome_sketch=sketch_dir, output=phylip_file, flag=conf["dnj_flag"])
+#kssdtree.build(phylip=phylip_file, output=newick_file, method=conf["dnj_method"])
+#kssdtree.visualize(newick=newick_file,  mode="r")
+kssdtree.quick(shuf_file=shuf_file+".shuf",genome_files=genomic_dir,output=newick_file,method="dnj")
